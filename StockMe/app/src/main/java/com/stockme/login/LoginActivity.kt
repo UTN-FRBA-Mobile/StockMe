d5b5ea09943d430047577f57da874da9532336bf
Rodrigo De Luca <rdeluca@thefork.com>
2021-10-28 00:26:07 -0300

    Lógica de signIn y reseteo de contraseña

--- StockMe/app/src/main/java/com/stockme/login/LoginActivity.kt
+++ StockMe/app/src/main/java/com/stockme/login/LoginActivity.kt
@@ -3,13 +3,21 @@ package com.stockme.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import com.stockme.R
 import com.stockme.databinding.ActivityLoginBinding
+import com.stockme.home.MainActivity_
+import com.stockme.login.viewmodel.LoginViewModel
 import com.stockme.register.RegisterActivity
+import com.stockme.utils.hideProgress
+import com.stockme.utils.showProgress
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel = LoginViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +25,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupViews()
+        setupObservers()
     }
 
     private fun setupViews() {
         binding.forgotPasswordText.setOnClickListener {
-            // Show dialog
+            showResetPasswordDialog()
         }
 
         binding.noAccountText.setOnClickListener {
@@ -33,9 +42,49 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun loginUser() {
-        if (areFieldsValid()) {
+    private fun setupObservers() {
+        viewModel.signInLiveData.observe(this) {
+            hideProgress()
+            if (it) {
+                MainActivity_.intent(this).start()
+                finish()
+            } else {
+                showSnackBar(getString(R.string.login_error))
+            }
+        }
 
+        viewModel.passwordResetLiveData.observe(this) {
+            hideProgress()
+            if (it) {
+                showDialog()
+            } else {
+                showSnackBar(getString(R.string.login_error))
+            }
+        }
+    }
+
+    private fun showResetPasswordDialog() {
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+
+        val alertDialogB = AlertDialog.Builder(this)
+
+        alertDialogB.apply {
+            setTitle(getString(R.string.login_forgot_password_dialog_title))
+            setView(input)
+            setPositiveButton(getString(R.string.login_forgot_password_dialog_positive_cta)) { _, _ ->
+                resetPassword(input.text.toString())
+            }
+            setNegativeButton(getString(R.string.login_forgot_password_dialog_negative_cta)) { dialog, _ ->
+                dialog.cancel()
+            }
+        }.create().show()
+    }
+
+    private fun loginUser() {
+        if (areLoginFieldsValid()) {
+            showProgress()
+            viewModel.loginUser(binding.loginEmailEditText.text.toString(), binding.loginPasswordEditText.text.toString())
         } else {
             showSnackBar(getString(R.string.login_invalid_parameters))
         }
@@ -46,7 +95,23 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun areFieldsValid(): Boolean = !binding.loginEmailEditText.text.isNullOrBlank() && !binding.loginPasswordEditText.text.isNullOrBlank()
+    private fun resetPassword(email: String) {
+        if (!email.trim().isNullOrBlank()) {
+            showProgress()
+            viewModel.resetPassword(email)
+        } else {
+            showSnackBar(getString(R.string.login_invalid_parameters))
+        }
+    }
+
+    private fun areLoginFieldsValid(): Boolean = !binding.loginEmailEditText.text.isNullOrBlank() && !binding.loginPasswordEditText.text.isNullOrBlank()
 
     private fun showSnackBar(text: String) = Snackbar.make(binding.root, text, Snackbar.LENGTH_LONG).show()
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.login_password_reset_successful))
+        builder.setPositiveButton(getString(R.string.login_forgot_password_dialog_positive_cta)) { dialog, _ -> dialog.cancel() }
+        builder.show()
+    }
 }
--- StockMe/app/src/main/java/com/stockme/login/viewmodel/LoginViewModel.kt
+++ StockMe/app/src/main/java/com/stockme/login/viewmodel/LoginViewModel.kt
@@ -1,4 +1,28 @@
 package com.stockme.login.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginViewModel: ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+    private val _signInLiveData = MutableLiveData<Boolean>()
+    private val _passwordResetLiveData = MutableLiveData<Boolean>()
+
+    val signInLiveData: MutableLiveData<Boolean> get() = _signInLiveData
+    val passwordResetLiveData: MutableLiveData<Boolean> get() = _passwordResetLiveData
+
+    fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            _signInLiveData.value = it.isSuccessful
+        }
+    }
+
+    fun resetPassword(email: String) {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            _passwordResetLiveData.value = it.isSuccessful
+        }
+    }
 }
