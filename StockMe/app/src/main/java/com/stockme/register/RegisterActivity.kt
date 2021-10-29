c89ea09d2ad2dd2d5ac51ac8ec49b064757e2c29
Rodrigo De Luca <rdeluca@thefork.com>
2021-10-29 18:29:20 -0300

    Reuso Snackbar

--- StockMe/app/src/main/java/com/stockme/register/RegisterActivity.kt
+++ StockMe/app/src/main/java/com/stockme/register/RegisterActivity.kt
@@ -2,13 +2,13 @@ package com.stockme.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import com.stockme.R
 import com.stockme.databinding.ActivityRegisterBinding
 import com.stockme.home.MainActivity_
 import com.stockme.register.viewmodel.RegisterViewModel
 import com.stockme.utils.hideProgress
 import com.stockme.utils.showProgress
+import com.stockme.utils.showSnackBar
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -37,7 +37,7 @@ class RegisterActivity : AppCompatActivity() {
                 MainActivity_.intent(this).start()
                 finish()
             } else {
-                showSnackBar(getString(R.string.register_error))
+                showSnackBar(binding.root, getString(R.string.register_error))
             }
         }
     }
@@ -49,7 +49,7 @@ class RegisterActivity : AppCompatActivity() {
                 binding.registerPasswordEditText.text.toString()
             )
         } else {
-            showSnackBar(getString(R.string.register_invalid_parameters))
+            showSnackBar(binding.root, getString(R.string.register_invalid_parameters))
         }
     }
 
@@ -59,6 +59,4 @@ class RegisterActivity : AppCompatActivity() {
             && !binding.registerRepeatPasswordEditText.text.trim().isNullOrBlank()
             && binding.registerPasswordEditText.text.toString() == binding.registerRepeatPasswordEditText.text.toString()
             && binding.registerCheckbox.isChecked
-
-    private fun showSnackBar(text: String) = Snackbar.make(binding.root, text, Snackbar.LENGTH_LONG).show()
 }
