74d26b2880df270bde1c1aeebc9d0dbede82c35f
Matias Dumrauf <matias@mixmax.com>
2021-11-29 12:19:49 -0300

    feat: implement sales fragment

--- StockMe/app/src/main/java/com/stockme/home/ui/sales/SalesFragment.kt
+++ StockMe/app/src/main/java/com/stockme/home/ui/sales/SalesFragment.kt
@@ -8,33 +8,88 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.stockme.R
 import com.stockme.databinding.FragmentSalesBinding
+import com.stockme.model.Product
+import com.stockme.product.ProductAdapter
+import com.stockme.product.RecyclerItemClickListener
 
 class SalesFragment : Fragment() {
 
-    private lateinit var salesViewModel: SalesViewModel
+    private lateinit var viewModel: SalesViewModel
     private var _binding: FragmentSalesBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var productAdapter: SalesProductAdapter
+
+    private val products = ArrayList<Product>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        salesViewModel =
+        viewModel =
             ViewModelProvider(this).get(SalesViewModel::class.java)
 
         _binding = FragmentSalesBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        salesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.productList.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+        binding.searchNotFound.root.visibility = View.GONE
+
+        viewModel.fetchProducts()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.productList.addOnItemTouchListener(
+                RecyclerItemClickListener(
+                        context,
+                        binding.productList,
+                        object : RecyclerItemClickListener.OnItemClickListener {
+                            override fun onItemClick(view: View?, position: Int) {
+//                                val product = productAdapter.productListFiltered[position]
+//                                navigateToDetail(product.id)
+                            }
+
+                            override fun onLongItemClick(view: View?, position: Int) { }
+                        })
+        )
+
+        setupObserver()
+    }
+
+
+    private fun setupObserver() {
+        viewModel.fetchProductsLiveData.observe(viewLifecycleOwner) {
+            binding.productList.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.GONE
+
+            if (it != null) {
+                products.clear()
+                products.addAll(it)
+                productAdapter = SalesProductAdapter(products)
+                binding.productList.apply {
+                    adapter = productAdapter
+                    layoutManager = LinearLayoutManager(context)
+                    addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+                    itemAnimator = DefaultItemAnimator()
+                }
+            } else {
+                Snackbar.make(binding.root, R.string.product_detail_error, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
