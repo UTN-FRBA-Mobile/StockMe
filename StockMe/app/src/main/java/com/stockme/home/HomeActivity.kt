cf95bd70afc96aabd90e66c9bd807674b682c1e6
Matias Dumrauf <matias@mixmax.com>
2021-11-29 03:06:14 -0300

    feat: implement logout

--- StockMe/app/src/main/java/com/stockme/home/HomeActivity.kt
+++ StockMe/app/src/main/java/com/stockme/home/HomeActivity.kt
@@ -35,7 +35,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_sales, R.id.nav_stocks
+                R.id.nav_home, R.id.nav_sales, R.id.nav_stocks, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
